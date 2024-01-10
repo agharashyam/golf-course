c5db11840b1dc79003906c00e715f742ffec2ea6
agharashyam <agharashyam2002@gmail.com>
2024-01-10 16:30:47 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+gsap.to("#nav",{
+    backgroundColor:"#000",
+    duration:0.5,
+    height:"120px",
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        // markers:true,
+        start:"top -10%",
+        end:"top -11%",
+        scrub:1,
+    }
+})
+gsap.to("#main",{
+    backgroundColor:"#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        // markers:true,
+        start:"top -30%",
+        end:"top -100%",
+        scrub:2,
+
+    }
+})
+gsap.from("#about-us img,#about-us-in",{
+    y:50,
+    opacity:0,
+    duration:1,
+    stagger:0.4,
+    scrollTrigger:{
+        trigger:"#about-us",
+        scroller:"body",
+        // markers:true,
+        start:"top 60%",
+        end:"top 55%",
+    }
+})
+gsap.from(".card",{
+    scale:0.8,
+    opacity:0,
+    duration:1,
+    scrollTrigger:{
+        trigger:".card",
+        scroller:"body",
+        start:"top 60%",
+        end:"top 55%",
+        scrub:2,  
+    }
+})
